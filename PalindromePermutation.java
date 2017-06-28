3e7f9e7fb9a4c4cf517bc32a78a9264c07130a77
Wally Faye <wally.faye@gmail.com>
2017-06-27 21:17:06 -0400

    init, compare string lengths

---
+++ PalindromePermutation.java
@@ -0,0 +1,11 @@
+public class PalindromePermutation{
+	public boolean res = true;
+	public PalindromePermutation(String str_1, String str_2){
+		if(str_1.length() != str_2.length()){
+			res = false;
+		}
+		System.out.println(str_1);
+		System.out.println(str_2);
+	}	
+
+}
