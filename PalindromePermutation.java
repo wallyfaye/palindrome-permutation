1bfc92af5d208826f458f094b9e7b4f1cbe7266d
Wally Faye <wally.faye@gmail.com>
2017-06-28 21:48:36 -0400

    temp solution for odd and even strings

--- PalindromePermutation.java
+++ PalindromePermutation.java
@@ -1,22 +1,25 @@
 public class PalindromePermutation{
-	public boolean res = true;
+	public boolean res = false;
 	public PalindromePermutation(String str_1){
 		int[] char_int = new int[512];
 		int debt = 0;
-		if(str_1.length()%2 == 0){
-			for(int i = 0; i < str_1.length(); i++){
-				if(char_int[str_1.charAt(i)] <= 0){
-					char_int[str_1.charAt(i)]++;
-					debt++;
-				} else {
-					char_int[str_1.charAt(i)]--;
-					debt--;
-				}
-			}
-			if(debt != 0){
-				res = false;
+
+		for(int i = 0; i < str_1.length(); i++){
+			if(char_int[str_1.charAt(i)] <= 0){
+				char_int[str_1.charAt(i)]++;
+				debt++;
+			} else {
+				char_int[str_1.charAt(i)]--;
+				debt--;
 			}
 		}
+
+		if( (debt == 0) && (str_1.length()%2 == 0) ){
+			res = true;
+		} else if( (debt == 1) && (str_1.length()%2 == 1) ){
+			res = true;
+		}
+
 	}	
 
 }
