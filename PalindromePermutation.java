b8a0a1d19a606f79dbf54a55a858d6f749f57d0f
Wally Faye <wally.faye@gmail.com>
2017-06-28 21:17:43 -0400

    compare characters in strings

--- PalindromePermutation.java
+++ PalindromePermutation.java
@@ -1,11 +1,24 @@
 public class PalindromePermutation{
 	public boolean res = true;
 	public PalindromePermutation(String str_1, String str_2){
-		if(str_1.length() != str_2.length()){
-			res = false;
-		}
-		System.out.println(str_1);
-		System.out.println(str_2);
+		// check if strings are the same length
+			if(str_1.length() != str_2.length()){
+				res = false;
+			}
+		
+		// check if the strings have the same characters
+			int[] perm_1 = new int[512];
+			for(int i = 0; i < str_1.length(); i++){
+				perm_1[str_1.charAt(i)]++;
+			}
+
+			for(int i = 0; i < str_2.length(); i++){
+				perm_1[str_2.charAt(i)]--;
+				if(perm_1[str_2.charAt(i)] < 0){
+					res = false;
+				}
+			}
+
 	}	
 
 }
